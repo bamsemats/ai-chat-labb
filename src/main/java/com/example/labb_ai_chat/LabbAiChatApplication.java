ab918cbdb693238000ac9c6a3f99805361eb80fd
Mats Rönnqvist <mats.f.ronnqvist@gmail.com>
2026-05-01 11:37:18 +0200

    first commit

---
+++ src/main/java/com/example/labb_ai_chat/LabbAiChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.labb_ai_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabbAiChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabbAiChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/labb_ai_chat/LabbAiChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labb_ai_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabbAiChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
